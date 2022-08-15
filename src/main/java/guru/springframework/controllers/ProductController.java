561b1cbacdeb2c9d8885f9bcbfa4fe148cd3e212
Ihor Horbonos <igorgorb@gmail.com>
2022-08-15 22:19:47 +0300

    modify ProductController for use ProductService

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,7 +1,11 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by jt on 1/20/16.
@@ -9,8 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
-    @RequestMapping({"/product", "/product.html"})
-    public String getProduct(){
-        return "product";
-    }
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @RequestMapping({"/product", "/product.html"})
+  public String getProduct() {
+    return "product";
+  }
+
+  @RequestMapping("/product/{id}")
+  public String getProductById(@PathVariable Integer id, Model model) {
+    model.addAttribute("product", productService.getProduct(id));
+    return "product";
+  }
 }
