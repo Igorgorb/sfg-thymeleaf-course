7244b3fd6f5fc4591320057b4a5260d8c4202c5c
Ihor Horbonos <igorgorb@gmail.com>
2022-08-15 23:46:25 +0300

    added iteration for displaying an objects

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String getIndex(){
+  private final ProductService productService;
 
-        return "index";
-    }
+  public IndexController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @RequestMapping("/")
+  public String getIndex(Model model) {
+
+    model.addAttribute("products", productService.listProducts());
+
+    return "index";
+  }
 }
