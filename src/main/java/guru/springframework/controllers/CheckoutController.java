92ae434ccb8d5e8ff1603afc215ca697d525f657
Ihor Horbonos <igorgorb@gmail.com>
2022-08-16 12:36:19 +0300

    added validation alert box

--- src/main/java/guru/springframework/controllers/CheckoutController.java
+++ src/main/java/guru/springframework/controllers/CheckoutController.java
@@ -3,6 +3,7 @@ package guru.springframework.controllers;
 import guru.springframework.comands.CheckoutCommand;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +16,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CheckoutController {
 
-  @RequestMapping("checkout")
-  public String checkoutForm() {
+  @RequestMapping("/checkout")
+  public String checkoutForm(Model model) {
+
+    model.addAttribute("checkoutCommand", new CheckoutCommand());
+    
     return "checkoutform";
   }
 
